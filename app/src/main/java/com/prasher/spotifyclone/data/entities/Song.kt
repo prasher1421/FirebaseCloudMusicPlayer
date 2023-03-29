ae9d4bf8bba71f01fdb46a001a5afd0ed753e84e
unknown <aryan1421g@gmail.com>
2023-03-30 00:27:59 +0530

    Firebase Setup

---
+++ app/src/main/java/com/prasher/spotifyclone/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.prasher.spotifyclone.data.entities
+
+data class Song(
+    val mediaID : String = "",
+    val title : String = "",
+    val songURL : String = "",
+    val imageURL : String = "",
+    val subtile : String = ""
+)
