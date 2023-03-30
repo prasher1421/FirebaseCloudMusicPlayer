6164d459d1098e2e0151c4f76fbb42ae13d1d5cf
unknown <aryan1421g@gmail.com>
2023-03-30 23:55:25 +0530

    Dagger Hilt Added

---
+++ app/src/main/java/com/prasher/spotifyclone/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.prasher.spotifyclone.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.prasher.spotifyclone.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(ApplicationComponentManager::class)//to give singleton objects whole app lifetime like glide
+object AppModule {
+
+    //Same instance is taken everytime
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+
+    )
+}
